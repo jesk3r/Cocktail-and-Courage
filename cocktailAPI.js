f359ef720f4a81966000a36b8ea9e0d0ebddf3ef
Shooterondeck77 <timothytaffur@gmail.com>
2023-08-25 17:06:14 -0400

    added module box

--- cocktailAPI.js
+++ cocktailAPI.js
@@ -57,7 +57,31 @@ function filters(data, fAlcoholic, fCategories, fglasses){
 }
 
 
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+function handleDrink() {
+  modal.style.display = 'block';
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 // NOT THE RIGHT WAY TO DO IT BUT TAKE A LOOK
 // var alcohol = document.querySelector(".alcohol");
