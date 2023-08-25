30d8371daefb68e9723b4e8cafc8cb55df019412
Dheeraj Thind <dheerajthind@gmail.com>
2023-08-25 10:42:25 -0400

    made a new website layout

--- cocktailAPI.js
+++ cocktailAPI.js
@@ -59,3 +59,46 @@ function filters(data, fAlcoholic, fCategories, fglasses){
 
 
 
+// NOT THE RIGHT WAY TO DO IT BUT TAKE A LOOK
+// var alcohol = document.querySelector(".alcohol");
+// var glass = document.querySelector(".glass");
+// var category = document.querySelector(".category");
+// var searchBar = document.querySelector(".searchBar");
+// var searchBtn = document.querySelector(".searchBtn");
+// let searchFilter;
+
+// alcohol.addEventListener("click", function () {
+//     searchFilter = "a";
+// });
+
+// glass.addEventListener("click", function () {
+//     searchFilter = "g";
+// });
+
+// category.addEventListener("click", function () {
+//     searchFilter = "c";
+// });
+
+// searchBtn.addEventListener("click", function () {
+//     var queryValue = encodeURIComponent(searchBar.value.trim());
+// var filterValue = searchFilter;
+//     fetchData(filterValue, queryValue)
+// });
+
+// function fetchData(filter, query) {
+//     let apiURrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?' + filter + '=' + query;
+//     console.log(apiURrl);
+//     fetch(apiURrl)
+//         .then(function (res) {
+//             return res.json();
+//         })
+//         .then(function (data) {
+//             console.log(data);
+//         })
+//         .catch(function (error) {
+//             console.error('Error fetching data:', error);
+//         });
+// };
+
+
+
